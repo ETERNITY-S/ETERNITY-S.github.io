2c35d8af9896a0159832a7dcde4efb813c2cc006
ETERNITY-S <shricon.tant@gmail.com>
2026-06-15 11:36:48 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,18 @@
+import { auth } from "./firebase.js";
+
+import {
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const logoutBtn =
+    document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", async () => {
+
+    await signOut(auth);
+
+    alert("Logged out successfully");
+
+    window.location.href = "index.html";
+
+});
