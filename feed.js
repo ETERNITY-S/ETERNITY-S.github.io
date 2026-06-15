8295adc246c917de0eff509440a0c7ac4658ce62
ETERNITY-S <shricon.tant@gmail.com>
2026-06-15 10:53:13 +0530

    Update feed.js

--- feed.js
+++ feed.js
@@ -9,14 +9,6 @@ import {
     getDoc
 } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-import {
-    collection,
-    addDoc,
-    getDocs,
-    orderBy,
-    query,
-    serverTimestamp
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
 let currentUser = null;
 let currentUsername = "";
